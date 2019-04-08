664b9557cb5412f7bccef358c6f80643aeba8a7c
NinoxDz <10012206+NinoxDz@users.noreply.github.com>
2019-04-08 13:53:18 +0200

    Update learn_utils.py

--- learn_utils.py
+++ learn_utils.py
@@ -52,18 +52,28 @@ def reconstruct(data, window, clusterer):
     Reconstruct the given data using the cluster centers from the given
     clusterer.
     """
-    window_len = len(window)
-    slide_len = window_len/2
-    segments = sliding_chunker(data, window_len, slide_len)
-    reconstructed_data = np.zeros(len(data))
-    for segment_n, segment in enumerate(segments):
-        # window the segment so that we can find it in our clusters which were
-        # formed from windowed data
-        segment *= window
-        nearest_match_idx = clusterer.predict(segment)[0]
-        nearest_match = np.copy(clusterer.cluster_centers_[nearest_match_idx])
 
-        pos = segment_n * slide_len
-        reconstructed_data[pos:pos+window_len] += nearest_match
+  window_len = len(window)
+  slide_len = int(window_len/2)
+  
+# data spliting with sliding lenght window_len/2 ( some datas are overlapping )
+  segments = sampling_data(data, window_len, slide_len)
+  
 
+  reconstructed_data = np.zeros(len(data))
+
+
+# find nearest centroid among clusters for reconstruction
+  for segment_n, segment in enumerate(segments):
+    segment *= window
+   
+    segment = np.reshape(segment,(1,window_len))
+    nearest_match_idx = clusterer.predict(segment)[0]
+    nearest_match = np.copy(clusterer.cluster_centers_[nearest_match_idx])
+  
+    pos = segment_n*slide_len
+    reconstructed_data[pos:pos+window_len] += nearest_match
+    
+    
     return reconstructed_data
+
